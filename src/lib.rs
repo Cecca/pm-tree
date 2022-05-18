57c7245ad7e0a9cf00c8c549ec06a09e5588c58d
Matteo Ceccarello <matteo.ceccarello@gmail.com>
2022-05-18 14:13:55 +0200

    Pass basic test

--- src/lib.rs
+++ src/lib.rs
@@ -1,56 +1,80 @@
 #![feature(drain_filter, new_uninit, maybe_uninit_uninit_array)]
 
+use std::marker::PhantomData;
+
 use serde_derive::{Deserialize, Serialize};
 
-const B: usize = 32;
+const B: usize = 4;
 
-#[derive(Serialize, Deserialize)]
-pub struct PMTree {
-    root: Box<Node>,
+pub trait Distance<T> {
+    fn distance(a: &T, b: &T) -> f64;
 }
 
-impl PMTree {
+#[derive(Serialize, Deserialize)]
+pub struct PMTree<T, D: Distance<T>> {
+    root: Box<Node<T, D>>,
+    _markert: PhantomData<T>,
+    _markerd: PhantomData<D>,
+}
+
+impl<T, D: Distance<T>> PMTree<T, D> {
     pub fn new() -> Self {
         Self {
             root: Box::new(Node::Leaf(LeafNode::new())),
+            _markert: PhantomData,
+            _markerd: PhantomData,
         }
     }
 
-    pub fn insert<T>(&mut self, o: usize, dataset: &[T],  dist: impl Fn(&T, &T) -> f64)
-    {
-        let possible_split = self.root.insert(o, None, dataset, &dist);
+    pub fn insert(&mut self, o: usize, dataset: &[T]) {
+        let possible_split = self.root.insert(o, None, dataset);
         if let Some((o1, n1, o2, n2)) = possible_split {
             // replace the current root with a new one
             let mut new_root = InnerNode::new(None);
-            new_root.do_insert(o1, Box::new(n1), None, dataset, &dist);
-            new_root.do_insert(o2, Box::new(n2), None, dataset, &dist);
+            new_root.do_insert(o1, Box::new(n1), None, dataset);
+            new_root.do_insert(o2, Box::new(n2), None, dataset);
             self.root = Box::new(Node::Inner(new_root));
         }
+    }
+
+    pub fn size(&self) -> usize {
+        self.root.size()
     }
 }
 
 #[derive(Serialize, Deserialize)]
-enum Node {
-    Inner(InnerNode),
-    Leaf(LeafNode),
+enum Node<T, D: Distance<T>> {
+    Inner(InnerNode<T, D>),
+    Leaf(LeafNode<T, D>),
 }
 
-impl Node {
-    fn insert<T>(
+impl<T, D: Distance<T>> Node<T, D> {
+    fn insert(
         &mut self,
         o: usize,
         parent: Option<usize>,
         dataset: &[T],
-        dist: impl Fn(&T, &T) -> f64,
-    ) -> Option<(usize, Self, usize, Self)>
-    {
+    ) -> Option<(usize, Self, usize, Self)> {
         match self {
             Node::Inner(inner) => inner
-                .insert(o, parent, dataset, dist)
+                .insert(o, parent, dataset)
                 .map(|(o1, n1, o2, n2)| (o1, Self::Inner(n1), o2, Self::Inner(n2))),
             Node::Leaf(leaf) => leaf
-                .insert(o, parent, dataset, dist)
+                .insert(o, parent, dataset)
                 .map(|(o1, n1, o2, n2)| (o1, Self::Leaf(n1), o2, Self::Leaf(n2))),
+        }
+    }
+
+    /// compute the size of this subtree
+    fn size(&self) -> usize {
+        match self {
+            Self::Inner(inner) => inner
+                .children
+                .iter()
+                .take(inner.len)
+                .map(|c| c.as_ref().unwrap().size())
+                .sum(),
+            Self::Leaf(leaf) => leaf.len,
         }
     }
 
@@ -74,15 +98,13 @@ impl Node {
     /// Computes an upper bound to the radius of the ball
     /// centered at the given router, containing all the nodes of the subtree
     /// rooted at the given node.
-    fn new_radius<T, D>(&self, router: usize, dataset: &[T], dist: D) -> f64
-    where
-        D: Fn(&T, &T) -> f64,
-    {
+    fn new_radius(&self, router: usize, dataset: &[T]) -> f64 {
         match self {
             Node::Inner(inner) => {
                 let mut r = 0.0;
                 for i in 0..inner.len {
-                    let d = dist(&dataset[inner.routers[i]], &dataset[router]) + inner.radius[i];
+                    let d =
+                        D::distance(&dataset[inner.routers[i]], &dataset[router]) + inner.radius[i];
                     if d > r {
                         r = d;
                     }
@@ -92,7 +114,7 @@ impl Node {
             Node::Leaf(leaf) => {
                 let mut r = 0.0;
                 for i in 0..leaf.len {
-                    let d = dist(&dataset[leaf.elements[i]], &dataset[router]);
+                    let d = D::distance(&dataset[leaf.elements[i]], &dataset[router]);
                     if d > r {
                         r = d;
                     }
@@ -104,44 +126,42 @@ impl Node {
 }
 
 #[derive(Serialize, Deserialize)]
-struct InnerNode {
+struct InnerNode<T, D: Distance<T>> {
     len: usize,
     parent: Option<usize>,
     parent_distance: [Option<f64>; B],
     routers: [usize; B],
     radius: [f64; B],
-    children: [Option<Box<Node>>; B],
+    children: [Option<Box<Node<T, D>>>; B],
+    _markert: PhantomData<T>,
+    _markerd: PhantomData<D>,
 }
 
-impl InnerNode {
+impl<T, D: Distance<T>> InnerNode<T, D> {
     fn new(parent: Option<usize>) -> Self {
-        // let children : [ Option<Box<Node<B>>>; B] = unsafe {
-        //     std::mem::transmute([0usize; B])
-        // };
+        let children: [Option<Box<Node<T, D>>>; B] = unsafe { std::mem::transmute([0usize; B]) };
         Self {
             len: 0,
             parent,
             parent_distance: [None; B],
             routers: [0; B],
             radius: [0.0; B],
-            children: todo!(),
+            children,
+            _markert: PhantomData,
+            _markerd: PhantomData,
         }
     }
 
-    fn insert<T, D>(
+    fn insert(
         &mut self,
         o: usize,
         parent: Option<usize>,
         dataset: &[T],
-        dist: D,
-    ) -> Option<(usize, Self, usize, Self)>
-    where
-        D: Fn(&T, &T) -> f64,
-    {
+    ) -> Option<(usize, Self, usize, Self)> {
         // find the routing element closest
         let closest = (0..self.len)
             .map(|i| {
-                let d = dist(&dataset[i], &dataset[o]);
+                let d = D::distance(&dataset[i], &dataset[o]);
                 (i, d)
             })
             .min_by(|p1, p2| p1.1.partial_cmp(&p2.1).unwrap());
@@ -151,12 +171,13 @@ impl InnerNode {
                 self.children[closest]
                     .as_mut()
                     .unwrap()
-                    .insert(o, Some(closest), dataset, &dist);
+                    .insert(o, Some(closest), dataset);
 
             if let Some((o1, n1, o2, n2)) = possible_split {
                 // replace node `closest` with o1
                 if let Some(parent) = self.parent {
-                    self.parent_distance[closest].replace(dist(&dataset[o1], &dataset[parent]));
+                    self.parent_distance[closest]
+                        .replace(D::distance(&dataset[o1], &dataset[parent]));
                 }
                 self.routers[closest] = o1;
                 self.children[closest].replace(Box::new(n1));
@@ -164,10 +185,10 @@ impl InnerNode {
 
                 // now, insert the new node directly, if possible, otherwise split the current node
                 if self.len < B {
-                    self.do_insert(o2, Box::new(n2), self.parent, dataset, &dist);
+                    self.do_insert(o2, Box::new(n2), self.parent, dataset);
                     None
                 } else {
-                    Some(self.split(o2, Box::new(n2), dataset, &dist))
+                    Some(self.split(o2, Box::new(n2), dataset))
                 }
             } else {
                 None
@@ -177,34 +198,28 @@ impl InnerNode {
         }
     }
 
-    fn do_insert<T, D>(
+    fn do_insert(
         &mut self,
         o: usize,
-        child: Box<Node>,
+        child: Box<Node<T, D>>,
         parent: Option<usize>,
         dataset: &[T],
-        dist: D,
-    ) where
-        D: Fn(&T, &T) -> f64,
-    {
+    ) {
         assert!(self.len < B);
         self.routers[self.len] = o;
         self.children[self.len].replace(child);
-        self.parent_distance[self.len] = parent.map(|parent| dist(&dataset[o], &dataset[parent]));
+        self.parent_distance[self.len] =
+            parent.map(|parent| D::distance(&dataset[o], &dataset[parent]));
         // TODO: update radius
         self.len += 1;
     }
 
-    fn split<T, D>(
+    fn split(
         &mut self,
         o2: usize,
-        child: Box<Node>,
+        child: Box<Node<T, D>>,
         dataset: &[T],
-        dist: D,
-    ) -> (usize, Self, usize, Self)
-    where
-        D: Fn(&T, &T) -> f64,
-    {
+    ) -> (usize, Self, usize, Self) {
         assert!(self.len == B);
         let o1 = self.routers[0];
         let data1 = &dataset[o1];
@@ -212,9 +227,11 @@ impl InnerNode {
         let mut n1 = Self::new(Some(o1));
         let mut n2 = Self::new(Some(o2));
 
-        n2.do_insert(o2, child, Some(o2), dataset, &dist);
+        n2.do_insert(o2, child, Some(o2), dataset);
         for i in 0..self.len {
-            if dist(&dataset[self.routers[i]], data1) < dist(&dataset[self.routers[i]], data2) {
+            if D::distance(&dataset[self.routers[i]], data1)
+                < D::distance(&dataset[self.routers[i]], data2)
+            {
                 // add to first node
                 assert!(n1.len < B);
                 n1.do_insert(
@@ -222,7 +239,6 @@ impl InnerNode {
                     self.children[i].take().unwrap(),
                     Some(o1),
                     dataset,
-                    &dist,
                 );
             } else {
                 // add to second node
@@ -232,7 +248,6 @@ impl InnerNode {
                     self.children[i].take().unwrap(),
                     Some(o2),
                     dataset,
-                    &dist,
                 );
             }
         }
@@ -242,54 +257,49 @@ impl InnerNode {
 }
 
 #[derive(Serialize, Deserialize)]
-struct LeafNode {
+struct LeafNode<T, D: Distance<T>> {
     len: usize,
     parent_distance: [Option<f64>; B],
     elements: [usize; B],
+    _markert: PhantomData<T>,
+    _markerd: PhantomData<D>,
 }
 
-impl LeafNode {
+impl<T, D: Distance<T>> LeafNode<T, D> {
     fn new() -> Self {
         Self {
             len: 0,
             parent_distance: [None; B],
             elements: [0; B],
+            _markert: PhantomData,
+            _markerd: PhantomData,
         }
     }
 
-    fn insert<T, D>(
+    fn insert(
         &mut self,
         o: usize,
         parent: Option<usize>,
         dataset: &[T],
-        dist: D,
-    ) -> Option<(usize, Self, usize, Self)>
-    where
-        D: Fn(&T, &T) -> f64,
-    {
+    ) -> Option<(usize, Self, usize, Self)> {
         if self.len == B {
-            Some(self.split(o, dataset, dist))
+            Some(self.split(o, dataset))
         } else {
-            self.do_insert(o, parent, dataset, dist);
+            self.do_insert(o, parent, dataset);
             None
         }
     }
 
-    fn do_insert<T, D>(&mut self, o: usize, parent: Option<usize>, dataset: &[T], dist: D)
-    where
-        D: Fn(&T, &T) -> f64,
-    {
+    fn do_insert(&mut self, o: usize, parent: Option<usize>, dataset: &[T]) {
         assert!(self.len < B);
-        self.parent_distance[self.len] = parent.map(|parent| dist(&dataset[o], &dataset[parent]));
+        self.parent_distance[self.len] =
+            parent.map(|parent| D::distance(&dataset[o], &dataset[parent]));
         self.elements[self.len] = o;
         self.len += 1;
     }
 
     /// Split the current node, leaving it empty, and returns two new nodes with the corresponding routing points
-    fn split<T, D>(&mut self, o2: usize, dataset: &[T], dist: D) -> (usize, Self, usize, Self)
-    where
-        D: Fn(&T, &T) -> f64,
-    {
+    fn split(&mut self, o2: usize, dataset: &[T]) -> (usize, Self, usize, Self) {
         assert!(self.len == B);
         let o1 = self.elements[0];
         let data1 = &dataset[o1];
@@ -297,18 +307,18 @@ impl LeafNode {
         let mut n1 = Self::new();
         let mut n2 = Self::new();
 
-        n2.insert(o2, Some(o2), dataset, &dist);
+        n2.insert(o2, Some(o2), dataset);
         for i in 0..self.len {
-            let d1 = dist(&dataset[self.elements[i]], data1);
-            let d2 = dist(&dataset[self.elements[i]], data2);
+            let d1 = D::distance(&dataset[self.elements[i]], data1);
+            let d2 = D::distance(&dataset[self.elements[i]], data2);
             if d1 < d2 {
                 // add to first node
                 assert!(n1.len < B);
-                n1.do_insert(self.elements[i], Some(o1), dataset, &dist);
+                n1.do_insert(self.elements[i], Some(o1), dataset);
             } else {
                 // add to second node
                 assert!(n2.len < B);
-                n2.do_insert(self.elements[i], Some(o2), dataset, &dist);
+                n2.do_insert(self.elements[i], Some(o2), dataset);
             }
         }
         self.len = 0;
@@ -317,14 +327,21 @@ impl LeafNode {
     }
 }
 
-pub fn euclidean(a: &Vec<f64>, b: &Vec<f64>) -> f64 {
-    assert!(a.len() == b.len());
-    a.iter().zip(b.iter()).map(|(a, b)| (a - b).powi(2)).sum()
+#[derive(Serialize, Deserialize)]
+pub struct Euclidean;
+
+impl Distance<Vec<f64>> for Euclidean {
+    fn distance(a: &Vec<f64>, b: &Vec<f64>) -> f64 {
+        assert!(a.len() == b.len());
+        a.iter().zip(b.iter()).map(|(a, b)| (a - b).powi(2)).sum()
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{PMTree, euclidean};
+    use crate::{Euclidean, PMTree};
+    use std::fs::File;
+    use std::io::prelude::*;
 
     #[test]
     fn it_works() {
@@ -341,10 +358,14 @@ mod tests {
             vec![-0.543183, 0.201419],
         ];
 
-        let mut pm_tree = PMTree::new();
-        pm_tree.insert(0, &dataset, euclidean);
+        let mut pm_tree = PMTree::<_, Euclidean>::new();
+        for i in 0..dataset.len() {
+            pm_tree.insert(i, &dataset);
+        }
 
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        let mut f = File::create("tree.json").unwrap();
+        writeln!(f, "{}", serde_json::to_string_pretty(&pm_tree).unwrap()).unwrap();
+
+        assert_eq!(pm_tree.size(), dataset.len());
     }
 }
